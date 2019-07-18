fe4f9d3a053f7a299e7d88ba51ea7eece4d6bd50
Hasan Khan <hasan.khan.uap@gmail.com>
2019-07-18 12:13:55 +0600

    react

--- Projects/hello-world/src/App.js
+++ Projects/hello-world/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hellow World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet name="Hasan" heroName="Batman">
+        <p>This is child props</p>  
+      </Greet>
+      <Welcome name="Hasan" heroName="Batman"/>
+      <Message /> */}
+      <Counter />
     </div>
   );
 }
