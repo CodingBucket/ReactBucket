afad05c8a6993e69b547464f28cc425f17f01cc7
Hasan Khan <hasan.khan.uap@gmail.com>
2019-07-22 01:25:12 +0600

    new

--- Projects/hello-world/src/App.js
+++ Projects/hello-world/src/App.js
@@ -4,16 +4,24 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
+import Style from './components/Style';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet name="Hasan" heroName="Batman">
-        <p>This is child props</p>  
-      </Greet>
-      <Welcome name="Hasan" heroName="Batman"/>
-      <Message /> */}
-      <Counter />
+      <Style/>
+      {/* <NameList/> */}
+      {/* <UserGreeting /> */}
+      {/* <ParentComponent/> */}
+      {/* <EventBind /> */}
+      {/* <Greet name="Hasan" heroName="Batman"><p>This is child props</p></Greet> */}
+      {/* <Welcome name="Hasan" heroName="Batman"/> */}
+      {/* <Message /> */}
+      {/* <Counter /> */}
     </div>
   );
 }
---
+++ Projects/hello-world/src/components/Style.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Style() {
+    return (
+        <div>
+            <h1>CSS</h1>
+        </div>
+    )
+}
+
+export default Style
