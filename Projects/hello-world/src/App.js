4d730d644849228b5299cb0a425a34841e47def2
Hasan Khan <hasan.khan.uap@gmail.com>
2019-07-27 10:41:10 +0600

    new

--- Projects/hello-world/src/App.js
+++ Projects/hello-world/src/App.js
@@ -12,12 +12,21 @@ import Style from './components/Style';
 import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
 import LifecycleB from './components/LifecycleB';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
+import FRParentInput from './components/ForwardRef/FRParentInput';
 
 function App() {
   return (
     <div className="App">
+      <FRParentInput />
+      {/* <FocusInput /> */}
+      {/* <RefsDemo /> */}
+      {/* <ParentComp /> */}
       {/* <LifecycleB/> */}
-      <LifecycleA/>
+      {/* <LifecycleA/> */}
       {/* <Form /> */}
       {/* <Style/> */}
       {/* <NameList/> */}
---
+++ Projects/hello-world/src/components/ForwardRef/FRInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// function FRInput() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+const FRInput = React.forwardRef(() => {
+    return(
+        <div></div>
+    )
+})
+
+export default FRInput
