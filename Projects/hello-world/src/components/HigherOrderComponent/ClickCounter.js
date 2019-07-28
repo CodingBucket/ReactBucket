0b07d2f3f0e55bb94028f2c0f6636987cd93fc82
Hasan Khan <hasan.khan.uap@gmail.com>
2019-07-29 01:12:09 +0600

    new

--- Projects/hello-world/src/components/HigherOrderComponent/ClickCounter.js
+++ Projects/hello-world/src/components/HigherOrderComponent/ClickCounter.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 class ClickCounter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count: 0
+        }
+    }
+    
+    incrementCount = () => {
+        this.setState(prevState => {
+            return { count: prevState.count + 1}
+        })
+    }
+
     render() {
         return (
             <div>
-                re
+                <button>Clicked X times</button>
             </div>
         )
     }
